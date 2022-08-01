ec0dd29d8753b2b3b82cc0a02919ae2e4a55e5d2
benNjogu <kamandemachafi@gmail.com>
2022-08-01 15:28:45 +0300

    fixed drawing problem

--- flappyBird.js
+++ flappyBird.js
@@ -1,12 +1,12 @@
-let cvs = document.getElementById("canvas");
-let ctx = cvs.getContext("2d");
+const cvs = document.getElementById("canvas");
+const ctx = cvs.getContext("2d");
 
 //load images
-let bird = new Image();
-let bg = new Image();
-let fg = new Image();
-let pipeNorth = new Image();
-let pipeSouth = new Image();
+const bird = new Image();
+const bg = new Image();
+const fg = new Image();
+const pipeNorth = new Image();
+const pipeSouth = new Image();
 
 bird.src = "images/bird.png";
 bg.src = "images/bg.png";
@@ -14,10 +14,18 @@ fg.src = "images/fg.png";
 pipeNorth.src = "images/pipeNorth.png";
 pipeSouth.src = "images/pipeSouth.png";
 
+//our variables
+let gap = 85;
+let constant = pipeNorth.height + gap;
+
 //draw images
 function draw() {
   ctx.drawImage(bg, 0, 0);
   ctx.drawImage(pipeNorth, 100, 0);
+  ctx.drawImage(pipeSouth, 100, 0 + constant);
+
+  ctx.drawImage(fg, 0, cvs.height - fg.height);
+  ctx.drawImage(bird, 10, 150);
 }
-alert("hello");
+alert("loading images");
 draw();
