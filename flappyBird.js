799f150d77525c6ad5f0ee8377b970d4a6b5eb3e
benNjogu <kamandemachafi@gmail.com>
2022-08-01 14:45:48 +0300

    fixed bug

--- flappyBird.js
+++ flappyBird.js
@@ -1,9 +1,9 @@
-var cvs = document.getElementById("canvas");
-var ctx = cvs.getContext("2d");
+let cvs = document.getElementById("canvas");
+let ctx = cvs.getContext("2d");
 
 //load images
 let bird = new Image();
-var bg = new Image();
+let bg = new Image();
 let fg = new Image();
 let pipeNorth = new Image();
 let pipeSouth = new Image();
@@ -11,5 +11,13 @@ let pipeSouth = new Image();
 bird.src = "images/bird.png";
 bg.src = "images/bg.png";
 fg.src = "images/fg.png";
-pipeNorth = "images/pipeNorth.png";
-pipeSouth = "images/pipeSouth.png";
+pipeNorth.src = "images/pipeNorth.png";
+pipeSouth.src = "images/pipeSouth.png";
+
+//draw images
+function draw() {
+  ctx.drawImage(bg, 0, 0);
+  ctx.drawImage(pipeNorth, 100, 0);
+}
+alert("hello");
+draw();
