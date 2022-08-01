769dfe3d1f4aaa2c0e847d1138c89009b6163a51
benNjogu <kamandemachafi@gmail.com>
2022-08-01 14:09:53 +0300

    load images

--- flappyBird.js
+++ flappyBird.js
@@ -1,1 +1,15 @@
+var cvs = document.getElementById("canvas");
+var ctx = cvs.getContext("2d");
 
+//load images
+let bird = new Image();
+var bg = new Image();
+let fg = new Image();
+let pipeNorth = new Image();
+let pipeSouth = new Image();
+
+bird.src = "images/bird.png";
+bg.src = "images/bg.png";
+fg.src = "images/fg.png";
+pipeNorth = "images/pipeNorth.png";
+pipeSouth = "images/pipeSouth.png";
